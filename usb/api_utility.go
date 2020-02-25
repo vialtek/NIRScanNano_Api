95b645cae204099a4a03a8ad52950daa7a93447b
David Filip <davidfilip@gmail.com>
2020-02-25 12:14:59 +0100

    Hibernate API implementation

---
+++ usb/api_utility.go
@@ -0,0 +1,32 @@
+package usb
+
+import (
+	"log"
+)
+
+func HibernateDevice() {
+	writeCommand(0x03, 0x0D, []byte{})
+}
+
+func SetHibernateMode(enabled bool) {
+	var enabledByte byte = 0
+	if enabled {
+		enabledByte = 1
+	}
+
+	writeCommand(0x03, 0x0E, []byte{enabledByte})
+}
+
+func HibernateMode() {
+	response := readCommand(0x03, 0x0F)
+	hibernate := response.payload[0]
+
+	log.Println("Hibernate Mode:", hibernate)
+}
+
+func PgaGain() {
+	response := readCommand(0x02, 0x28)
+	gain := response.payload[0]
+
+	log.Println("PGA Gain:", gain)
+}
